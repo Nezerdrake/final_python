b0e8a7559a8a68a232c60677bf1f08ac2c37f02d
Nezerdrake <nezerdrake@gmail.com>
2024-07-19 12:24:12 +0300

    завершенное задание

---
+++ dataframe.py
@@ -0,0 +1,18 @@
+import random
+import pandas as pd
+
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI': lst})
+# data.head()
+
+one_hot = pd.DataFrame()
+unique_values = data['whoAmI'].unique()
+
+for value in unique_values:
+    one_hot[value] = (data['whoAmI'] == value).astype(int)
+
+print(one_hot.head())
+# print(data.shape)
+# print(one_hot.shape)
